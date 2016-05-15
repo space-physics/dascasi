ec698e13c4fd88af6a96b0cb050fa077bbfd5b0f
michael <scienceopen@users.noreply.github.com>
2016-05-15 01:49:35 -0400

    init

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.run(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+    subprocess.run(['conda','install','--yes','--file','requirements.txt'])
 except Exception as e:
     print('you will need to install packages in requirements.txt  {}'.format(e))
 
