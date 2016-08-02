d86951a3dd99843227ea420b419a2aab85b4d906
scienceopen <scienceopen@users.noreply.github.com>
2016-08-02 14:53:42 -0400

    new ftp site

--- DownloadDASC.py
+++ DownloadDASC.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 from dascutils import Path
+from time import sleep
 import ftplib
 from urllib.parse import urlparse
 
-host = "ftp://mirrors.arsc.edu/AMISR/PKR/DASC/RAW/"
-
-def getdasc(year,month,day,hour,minute,odir='',clobber=False):
+def getdasc(year,month,day,hour,minute,host,site,odir='',clobber=False):
     """
     year,month,day: integer
     hour, minute:  start,stop integer len == 2
     """
     parsed = urlparse(host)
     ftop = parsed[1]
-    fpath = parsed[2]
+    fpath = parsed[2] + site
 
 #%% get available files for this day
     with ftplib.FTP(ftop,'anonymous','guest',timeout=15) as F:
-        rpath = fpath + str(year) + '/{:4d}{:02d}{:02d}'.format(year,month,day)
+        rpath = fpath + '/DASC/RAW/{}/{:4d}{:02d}{:02d}'.format(year,year,month,day)
         F.cwd(rpath)
         dlist = F.nlst()
 
@@ -35,6 +34,7 @@ def getdasc(year,month,day,hour,minute,odir='',clobber=False):
                 print(ofn)
                 with ofn.open('wb') as h:
                     F.retrbinary('RETR {}'.format(f), h.write)
+                    sleep(1)
 
 
 if __name__ == '__main__':
@@ -47,7 +47,11 @@ if __name__ == '__main__':
     p.add_argument('end',help='end time UTC e.g. 06:25')
     p.add_argument('-o','--odir',help='directory to write downloaded FITS to',default='')
     p.add_argument('-c','--clobber',help='clobber (overwrite) existing files',action='store_true')
+    p.add_argument('--host',default='ftp://optics.gi.alaska.edu')
+    p.add_argument('-s','--site',help='EAA FYU KAK PKR TOO VEE',default='PKR')
     P = p.parse_args()
+
+#host = "ftp://mirrors.arsc.edu/AMISR/PKR/DASC/RAW/"
 
     start = [int(i) for i in P.start.split(':')]
     end =   [int(i) for i in P.end.split(':')]
@@ -55,4 +59,4 @@ if __name__ == '__main__':
     hour = (start[0],end[0])
     minute = (start[1],end[1])
 
-    getdasc(P.year, P.month, P.day, hour, minute, P.odir,P.clobber)
+    getdasc(P.year, P.month, P.day, hour, minute, P.host,P.site, P.odir,P.clobber)
---
+++ downloadDASC.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# reference: https://scivision.co/wget-download-an-http-directory-recursively/
+
+date=$1
+year=${date:0:4}
+site=$2
+
+case "$site" in
+    PKR)
+    type="DASC";;
+    KAK)
+    type="DASC";;
+    TOO)
+    type="CASC";;
+esac
+
+wget -nc -nd -nH -r -np --no-check-certificate -A.FITS -A.FIT \
+ --random-wait --wait 1 -e robots=off \
+ ftp://optics.gi.alaska.edu/$site/$type/RAW/$year/$date
+
